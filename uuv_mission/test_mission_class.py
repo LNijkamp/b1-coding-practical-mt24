a06d50d6b465f72472742d5535cb5fbac3d5f6f2
Luke <luke.nijkamp@seh.ox.ac.uk>
2024-10-19 16:16:44 +0100

    Added test script for mission data

---
+++ uuv_mission/test_mission_class.py
@@ -0,0 +1,7 @@
+#a script to test the mission class change
+
+from dynamic import Mission
+
+mission = Mission.from_csv('/Users/lukenijkamp/Library/CloudStorage/OneDrive-Nexus365/Engineering 3rd year/B1 Engineering Computation/Scientific coding/B1 Practical 1/b1-coding-practical-mt24/data/mission.csv')
+        
+print(mission)
